6a115fc818b625240d795d82c705d4994b252c5a
hexxt-git <zeghdns@gmail.com>
2025-10-25 16:09:55 +0300

    new title font

--- frontend/src/routes/(app)/contracts.$id.tsx
+++ frontend/src/routes/(app)/contracts.$id.tsx
@@ -10,7 +10,7 @@ function RouteComponent() {
   return (
     <div className="grid grid-cols-[5fr_3fr] gap-4 h-[calc(95vh-4.5rem)] min-h-48">
       <div className="p-4 pb-6 border rounded-xl shadow-island bg-card">
-        <h2 className="text-lg font-semibold">Contract Details</h2>
+        <h2 className="text-2xl font-bold font-title">Contract Details</h2>
         <p className="text-sm text-muted-foreground max-w-xs">
           The contract details will be displayed here. I don't know what to put
           here yet.
